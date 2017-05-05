5689f5126693e6986d442147670083c8430c747a
Andy Price <andrewprice8487@yahoo.co.uk>
2017-05-05 21:11:41 +0100

    ttt 

--- src/app/shopping-list/shopping-list.component.ts
+++ src/app/shopping-list/shopping-list.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import {ingredient} from '../shared/ingredient.model';
 
 @Component({
   selector: 'app-shopping-list',
@@ -6,6 +7,12 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./shopping-list.component.css']
 })
 export class ShoppingListComponent implements OnInit {
+  ingredients: ingredient[] = [
+
+    new ingredient('Basil', 2),
+    new ingredient('Tomato', 5),
+    new ingredient('Garlic', 0.5),
+  ];
 
   constructor() { }
 
